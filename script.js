11090453186d5cfe30061e594af85b24d0dd7777
DmitryBavelko-98 <bavelko90@gmail.com>
2022-06-29 15:16:52 +0300

    Add local storage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const books = document.querySelector('.library__books');
 const addBtn = document.querySelector('.library__show-form');
-const popup = document.querySelector('.library__form-popup');
+const popup = document.querySelector('.library__overlay');
 const form = document.querySelector('.library__form');
 const title = document.querySelector('.library__title');
 const author = document.querySelector('.library__author');
@@ -23,38 +23,36 @@ Book.prototype.info = function () {
     console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`);
 } 
 
-function renderBooks(items) {
-    if ((title.value && author.value && pages.value)) {
-        books.innerHTML = '';
-        items.map((item, i, arr) => {
-            const book = document.createElement('div');
-            book.innerHTML = `
-                <div class="book__title">${item.title}</div>
-                <div class="book__author">${item.author}</div>
-                <div class="book__pages">${item.pages}</div>
-                <button class="book__read">${item.isRead ? 'Read' : 'Not read'}</button>
-                <button class="book__remove">Remove</button>
-            `;
-            books.append(book);
-            book.classList.add('library__book');
-    
-            const read = document.querySelectorAll('.book__read');
-            read.forEach(btn => {
-                btn.addEventListener('click', (e) => {
-                    item.isRead = !item.isRead;
-                    e.target.innerHTML = item.isRead ? 'Read' : 'Not read';
-                })
+function renderBooks(items, pos = 0) {
+    items.map((item, i) => {
+        const book = document.createElement('div');
+        book.innerHTML = `
+            <div class="book__title">${item.title}</div>
+            <div class="book__author">${item.author}</div>
+            <div class="book__pages">${item.pages}</div>
+            <button class="book__read">${item.isRead ? 'Read' : 'Not read'}</button>
+            <button class="book__remove">Remove</button>
+        `;
+        books.append(book);
+        book.classList.add('library__book');
+
+        const read = document.querySelectorAll('.book__read');
+        read.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                item.isRead = !item.isRead;
+                e.target.innerHTML = item.isRead ? 'Read' : 'Not read';
             })
-            
-            const remove = document.querySelectorAll('.book__remove');
-            remove.forEach(btn => {
-                btn.addEventListener('click', (e) => {
-                    arr.splice(i);
-                    e.target.closest('div').remove();
-                })
-            });
         })
-    }
+        
+        const remove = document.querySelectorAll('.book__remove');
+        remove.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                e.target.parentNode.remove();
+                myLibrary.splice(i, 1)
+                localStorage.setItem('library', JSON.stringify(myLibrary));
+            })
+        })
+    })
 }
 
 function openModal(trigger, modal, close) {
@@ -74,14 +72,24 @@ function openModal(trigger, modal, close) {
     })
 }
 
+
 function addBook(e) {
     if ((title.value && author.value && pages.value)) {
         const book = new Book(title.value, author.value, pages.value, isRead.checked);
         myLibrary.push(book);
-        renderBooks(myLibrary);
+        books.innerHTML = '';
         document.body.classList.remove('dark');
+        localStorage.setItem('library', JSON.stringify(myLibrary));
+        renderBooks(myLibrary);
     }
 }
+
+// localStorage.clear()
+
+window.addEventListener('DOMContentLoaded', () => {
+    myLibrary = JSON.parse(localStorage.getItem('library'))
+    renderBooks(myLibrary);
+})
 
 popup.addEventListener('click', (e) => {
     if (e.target === popup) {
